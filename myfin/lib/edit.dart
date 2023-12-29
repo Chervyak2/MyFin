4eb3771ee9d901ba9b0307f2a510ba67cbd3a63f
Chervyak2 <106195564+Chervyak2@users.noreply.github.com>
2023-12-30 02:57:53 +0300

    Chervyak (#10)
    * progres
    
    * change new and edit.dart
    
    * change edit

--- myfin/lib/edit.dart
+++ myfin/lib/edit.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:myfin/date_widget.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter/services.dart';
 
-class NewScreen extends StatefulWidget {
-  const NewScreen({Key? key}) : super(key: key);
+class EditScreen extends StatefulWidget {
+  const EditScreen({Key? key}) : super(key: key);
 
   @override
-  _NewScreenState createState() => _NewScreenState();
+  _EditScreenState createState() => _EditScreenState();
 }
 
-class _NewScreenState extends State<NewScreen> {
-  bool _showFirstInfo = true;
+class _EditScreenState extends State<EditScreen> {
   final _firstInfoController1 = TextEditingController();
   final _firstInfoController2 = TextEditingController();
   final _firstInfoController3 = TextEditingController();
-  final _firstInfoController4 = TextEditingController();
-  final _secondInfoController1 = TextEditingController();
+  final _firstInfoController4 = TextEditingController(); // Добавлено здесь
 
   @override
   Widget build(BuildContext context) {
@@ -63,30 +62,13 @@ class _NewScreenState extends State<NewScreen> {
               height: 50, // Изменено здесь
               color: Color.fromRGBO(20, 20, 20, 1),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _showFirstInfo = true;
-                      });
-                    },
-                    child: Text('New record',
+                  Padding(
+                    padding: EdgeInsets.only(left: 16.0), // Отступ слева
+                    child: Text('Name:',
                         style: TextStyle(
-                            color: _showFirstInfo ? Colors.white : Colors.grey,
-                            fontFamily: 'Poppins',
-                            fontSize: 24)),
-                  ),
-                  SizedBox(width: 30),
-                  TextButton(
-                    onPressed: () {
-                      setState(() {
-                        _showFirstInfo = false;
-                      });
-                    },
-                    child: Text('New category',
-                        style: TextStyle(
-                            color: _showFirstInfo ? Colors.grey : Colors.white,
+                            color: Colors.white,
                             fontFamily: 'Poppins',
                             fontSize: 24)),
                   ),
@@ -94,13 +76,14 @@ class _NewScreenState extends State<NewScreen> {
               ),
             ),
             SizedBox(height: 40), // Добавлено здесь
-            _showFirstInfo
-                ? FirstInfo(_firstInfoController1, _firstInfoController2,
-                    _firstInfoController3, _firstInfoController4)
-                : SecondInfo(_secondInfoController1),
+            FirstInfo(
+                _firstInfoController1,
+                _firstInfoController2,
+                _firstInfoController3,
+                _firstInfoController4), // Добавлено здесь
             Container(
-              height: 55,
-              width: 200,
+              height: 60,
+              width: 220,
               decoration: BoxDecoration(
                 color: Colors.black,
                 borderRadius:
@@ -175,109 +158,6 @@ class _FirstInfoState extends State<FirstInfo> {
           _buildTextField('\$30.00', 'Amount', controller3),
           SizedBox(height: 10),
           _buildTextField('24.11.2023', 'Date', controller4),
-          SizedBox(height: 10),
-          _buildSwitch('Type'),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTextField(
-      String hint, String info, TextEditingController controller) {
-    return Padding(
-      padding: EdgeInsets.only(
-          right: 10.0, left: 20.0), // Добавлены отступы справа и слева
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(info, style: TextStyle(color: Colors.white, fontSize: 18.0)),
-          Container(
-            width: 260.0,
-            height: 45.0,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: controller,
-                decoration: InputDecoration(
-                  hintText: hint,
-                  hintStyle: TextStyle(
-                      color: Colors.grey), // Изменен цвет подсказки на серый
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 44, 44, 44),
-                  contentPadding: EdgeInsets.only(left: 10.0, bottom: 10.0),
-                ),
-                style: TextStyle(fontSize: 18.0, color: Colors.white),
-                textAlignVertical: TextAlignVertical.center,
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildSwitch(String info) {
-    return Padding(
-      padding: EdgeInsets.only(left: 20.0), // Добавлен отступ слева
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(info, style: TextStyle(color: Colors.white, fontSize: 18.0)),
-          Transform.scale(
-            scale: 1 / 1.8, // Уменьшение размера в 1.8 раза
-            child: Switch(
-              value: switchValue,
-              onChanged: (value) {
-                setState(() {
-                  switchValue = value;
-                });
-              },
-              activeColor:
-                  const Color.fromARGB(255, 184, 118, 81), // Цвет ползунка
-              activeTrackColor:
-                  const Color.fromARGB(255, 26, 26, 26), // Цвет полосы
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 110.0), // Отступ справа
-            child: Text(
-              switchValue
-                  ? 'Expense'
-                  : 'Income', // Изменен текст в зависимости от состояния переключателя
-              style: TextStyle(fontSize: 18.0, color: Colors.white),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class SecondInfo extends StatefulWidget {
-  final TextEditingController controller1;
-
-  SecondInfo(this.controller1);
-
-  @override
-  _SecondInfoState createState() => _SecondInfoState(controller1);
-}
-
-class _SecondInfoState extends State<SecondInfo> {
-  final TextEditingController controller1;
-  bool switchValue = true;
-
-  _SecondInfoState(this.controller1);
-
-  @override
-  Widget build(BuildContext context) {
-    double thirdScreenHeight = MediaQuery.of(context).size.height / 3 + 60;
-
-    return Container(
-      height: thirdScreenHeight,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          _buildTextField('Shopping', 'Name', controller1),
           SizedBox(height: 10),
           _buildSwitch('Type'),
         ],
